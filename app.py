4e0717cf6ef701da679c4db0ccaba24e27e49c2a
Aline Ponciano Dos Santos <aline.ponciano@pucpr.edu.br>
2025-08-26 15:24:32 -0300

    adicionar função saudacao
    Esse commit adiciona a função saudacao ao app python

--- app.py
+++ app.py
@@ -2,3 +2,8 @@
 print("Ola! Esta é minha primeira aplicação em Python.")
 
 print("Commit 2: Estou aprendendo python passo a passo!")
+
+nome = "Aline"
+
+def saudacao(nome):
+    return f'Olá {nome}! Bem-Vindo ao Python!!'
