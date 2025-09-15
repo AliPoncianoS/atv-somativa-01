bc4b141e6724b6cd9d690af9fedb316471ca2cad
Aline Ponciano Dos Santos <aline.ponciano@pucpr.edu.br>
2025-09-15 14:39:24 -0300

    atuzlização

--- test/test_app.py
+++ test/test_app.py
@@ -1,5 +1,5 @@
-# tests/test_app.py
-from app import saudacao
+# test/test_app.py
+from src.app import saudacao
 
 def test_saudacao():
     assert saudacao("Aline") == "Olá Aline! Bem-Vindo ao Python!!"
--- tests/test_app.py
+++
@@ -1,5 +0,0 @@
-# tests/test_app.py
-from app import saudacao
-
-def test_saudacao():
-    assert saudacao("Aline") == "Olá Aline! Bem-Vindo ao Python!!"
